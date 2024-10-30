c73d2a72cb2d2a5c45763f8a0763c8ad8e1e3a1b
NhatHienUIT <162800174+NhatHienUIT@users.noreply.github.com>
2024-10-31 01:04:20 +0700

    Update convert_dataset.py

--- convert_dataset.py
+++ convert_dataset.py
@@ -27,7 +27,7 @@ def create_webdataset(base_pattern, dataset, m, s, n, names):
             print (data.size())
             # Convert torch.Tensor to numpy.ndarray and transpose to H x W x C
             if isinstance(data, torch.Tensor):
-                data = data.permute(1, 2, 0).numpy()  # Transpose from C x H x W to H x W x C
+                data = data.permute(1, 2, 0).detach().numpy()  # Transpose from C x H x W to H x W x C
 
             sample = {
                 "__key__": key,
