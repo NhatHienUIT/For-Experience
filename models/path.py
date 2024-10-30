e06ea8c5739af5c4b7337a0f101de3112c060097
NhatHienUIT <162800174+NhatHienUIT@users.noreply.github.com>
2024-10-30 23:27:43 +0700

    Update path.py

--- models/path.py
+++ models/path.py
@@ -24,7 +24,7 @@ class Net(nn.Module):
 def classifier_path():
     return Net()
 
-def robustifier_pathmnist(x_min, x_max, x_avg, x_std, x_epsilon_defense):
+def robustifier_path(x_min, x_max, x_avg, x_std, x_epsilon_defense):
     convolutional_dnn = nn.Sequential(
         nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1),
         nn.ReLU(),
