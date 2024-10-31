2379b765e6547b71f00f39322dcabc201aacc25f
NhatHienUIT <162800174+NhatHienUIT@users.noreply.github.com>
2024-10-31 17:05:53 +0700

    Update path.py

--- models/path.py
+++ models/path.py
@@ -4,25 +4,33 @@ import torch.nn.functional as F
 from models.robustifier import Robustifier
 
 class Net(nn.Module):
-    def __init__(self, num_classes=9): 
+    def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(128 * 3 * 3, 128)  
-        self.fc2 = nn.Linear(128, num_classes)
-        
+
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1)),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(128 * 28 * 28, 512),  # Adjust this linear layer based on input image size
+            nn.ReLU(),
+            nn.Linear(512, 9)  # PathMNIST has 9 classes
+        )
+
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
-        x = x.view(-1, 128 * 3 * 3)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
-def classifier_path():
+        logits = self.model(x)
+        return logits
+
+def classifier_pathmnist():
     return Net()
+
 
 def robustifier_path(x_min, x_max, x_avg, x_std, x_epsilon_defense):
     convolutional_dnn = nn.Sequential(
