304400f6ab057c12d7eeec27cfa20896591c694a
NhatHienUIT <162800174+NhatHienUIT@users.noreply.github.com>
2024-11-01 08:01:13 +0700

    Update convert_dataset.py

--- convert_dataset.py
+++ convert_dataset.py
@@ -27,11 +27,6 @@ def create_webdataset(base_pattern, dataset, m, s, n, names):
 
         for i, (data, label) in enumerate(dataset):
             key = "%.6d" % i
-
-            # Convert torch.Tensor to numpy.ndarray and transpose to H x W x C
-            if isinstance(data, torch.Tensor):
-                data = data.permute(1, 2, 0).cpu().detach().numpy()
-                print(f"Sample {i} - Data shape after transpose: {data.shape}")
             label = label.item() if isinstance(label, torch.Tensor) else int(label[0])
             sample = {
                 "__key__": key,
